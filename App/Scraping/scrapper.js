37aaf9fde414d14d48ff0a6b77b6ed2ef8ec5211
louicoder <musanje2010@gmail.com>
2019-02-04 12:55:32 +0300

    re-arrange app folder structure

--- App/Scraping/scrapper.js
+++ App/Scraping/scrapper.js
@@ -1,7 +1,7 @@
 // THIS FILE IS RESPONSIBLE FOR RUNNING THE SERVER REQUESTS
 
 const cheerio = require('cheerio');
-const fs = require('fs');
+// const fs = require('fs'); // uncomment if you need to use this module
 const request = require('request');
 
 // class created for purposes of creating instances. But this as well can be ommitted and functions can be placed directly in this file and exported for use in other files.
