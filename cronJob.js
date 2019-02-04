1698ab1a15617a0d5572af6df9a7635862e9cec5
louicoder <musanje2010@gmail.com>
2019-02-04 12:16:26 +0300

    refactor cronjob functionality for modularity

--- cronJob.js
+++ cronJob.js
@@ -4,10 +4,23 @@ const fs = require("fs");
 
 app = express();
 
-// lets schedule task to be run on the server
-
-cron.schedule("20 * * * *", function () {
+// lets schedule task through a function that can be exportedd to other modules.
+const cronJob = () => cron.schedule("20 * * * *", function () {
     console.log("running a task every 20 seconds");
 });
 
+// DECSRIPTION OF THE CRON TIMING ARGUMENTS.
+// --------------------------- second (optional)
+// \    ---------------------- minute
+// \    \   ------------------ hour
+// \    \   \   -------------- day o month
+// \    \   \   \   ---------- month
+// \    \   \   \   \   ------ day of week
+// \    \   \   \   \   \
+// \    \   \   \   \   \
+// *    *   *   *   *   *
+
 app.listen(3128);
+
+module.exports = cronJob;
+
