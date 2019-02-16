8f5ad5aa48f466247077608ea50efc9ed12a3a89
louiCoder <musanje2010@gmail.com>
2019-02-16 13:51:21 +0300

    heroku fixes

--- App/Whatsapp/numbers.js
+++ App/Whatsapp/numbers.js
@@ -1,5 +1,8 @@
 const whatsappNumbers = [
-    '+256704506345'
+    '+256704506345', // Louis
+    '+2349052630413', // Kenneth
+    '+256704740595', // Frank
+    '+256750461002' // Patrick
 ]
 
 module.exports = whatsappNumbers;
