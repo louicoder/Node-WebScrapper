1f6968832fff64c2c25f78f3fd67c3ccd312ada8
kenneth okenwa <okenwanonsokenneth@gmail.com>
2019-02-20 21:08:53 +0100

    little changes

--- App/Files/FilesUtility/readAndWriteToFiles.js
+++ App/Files/FilesUtility/readAndWriteToFiles.js
@@ -1,13 +1,36 @@
-function* writeToTextFile() {
-    fs.writeFile('Output.txt', data, (err) => {
+const fs = require("fs");
 
-        // In case of a error throw err. 
-        if (err) throw err;
+const content = "some content!";
+
+const path = "./Output.txt";
+
+//lets see how to creat a function that will write the file
+function* writeToTextFile() {
+    fs.writeFile('./Output.txt', content, (err) => {
+        // if an error occurs 
+        if (err) {
+
+            console.log(err);
+
+        }
+        else {
+            // if no error
+            console.log("file written successfully");
+        }
+
     })
 }
 
 function readFromTextFile() {
+    // read from file
+    fs.readFile('./Output.txt', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        content = data;
+    });
 
 }
 
+writeToTextFile(content, path);
 module.exports = { writeToTextFile, readFromTextFile }
